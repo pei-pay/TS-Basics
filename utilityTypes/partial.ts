aa4353ccc843183e2df8af02eb9a6c2e0086dd69
pei-pay <gleektennis@icloud.com>
2022-10-10 12:19:38 +0900

    add: utilityTypes description

--- utilityTypes/partial.ts
+++ utilityTypes/partial.ts
@@ -1,3 +1,7 @@
+/**
+ * Constructs a type with all properties of Type set to optional. This utility will return a type that represents all subsets of a given type.
+ */
+
 interface Todo {
   title: string;
   description: string;
